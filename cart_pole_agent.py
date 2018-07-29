76ff4741b3a12d4f7e9aa6c72061eb3cde6a68ea
Harry J. Braviner <harry.braviner@gmail.com>
2018-07-29 19:14:18 +0100

    Bug fixes
    Mostly fixed typos I made.

--- cart_pole_agent.py
+++ cart_pole_agent.py
@@ -30,16 +30,16 @@ def discount_rewards(r):
     return discounted_r
 
 # Define the agent
-agent_state_in = tf.placeholder([None, state_size], dtype=tf.float32)
+agent_state_in = tf.placeholder(shape=[None, state_size], dtype=tf.float32)
 agent_W1 = tf.Variable(tf.truncated_normal([state_size, hidden_size], mean = 0.0, stddev = 1.0))
 agent_b1 = tf.Variable(tf.zeros([hidden_size]))
-agent_h = tf.nn.relu(agent_state_in * agent_W1 + agent_b1)
+agent_h = tf.nn.relu(tf.matmul(agent_state_in, agent_W1) + agent_b1)
 agent_W2 = tf.Variable(tf.truncated_normal([hidden_size, action_size], mean = 0.0, stddev = 1.0))
 agent_b2 = tf.Variable(tf.zeros([action_size]))
-agent_output = tf.nn.softmax(agent_h * agent_W2 + agent_h2)
+agent_output = tf.nn.softmax(tf.matmul(agent_h, agent_W2) + agent_b2)
 
 agent_discounted_rewards = tf.placeholder(shape = [None], dtype = tf.float32)
-agent_chosen_actions = tf.placeholder(shape = [None], dtype = tf.float32)
+agent_chosen_actions = tf.placeholder(shape = [None], dtype = tf.int32)
 
 agent_indices = tf.range(0, tf.shape(agent_output)[0]) * tf.shape(agent_output)[1] + agent_chosen_actions
 agent_responsible_outputs = tf.gather(tf.reshape(agent_output, [-1]), agent_indices)
@@ -50,12 +50,12 @@ agent_trainable_vars = tf.trainable_variables() # The variables we'll apply grad
 
 agent_grad_holders = [] # Will be used to pass in the values for the gradients
 for idx,var in enumerate(agent_trainable_vars):
-    placeholder = tf.placeholder(tf.float32, name = idx + "_holder")
+    placeholder = tf.placeholder(tf.float32, name = str(idx) + "_holder")
     agent_grad_holders.append(placeholder)
 
 agent_gradients = tf.gradients(agent_loss, agent_trainable_vars)
 agent_optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-agent_update_batch = agent_optimizer.apply_gradients(zip(agent_gradients, agent_trainable_vars))
+agent_update_batch = agent_optimizer.apply_gradients(zip(agent_grad_holders, agent_trainable_vars))
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
@@ -73,27 +73,26 @@ for ix, grad in enumerate(grad_buffer):
 while ep < num_episodes:
     state = env.reset()
     this_ep_history = []
+    reward_this_ep = 0.0
 
     for j in range(max_ep_length):
 
-        reward_this_ep = 0.0
-
-        action_dist = sess.run(agent_output, feed_dict = {agent_state_in : state})
+        action_dist = sess.run(agent_output, feed_dict = {agent_state_in : [state]})
         chosen_action = np.random.choice(action_dist[0], p = action_dist[0])
-        chosen_action = np.argmax(action_dist == chosen_action) # FIXME - I'm really not sure I get his line
+        chosen_action = np.argmax(action_dist == chosen_action) # FIXME - I'm really not sure I get this line
 
         s1, r, d, _ = env.step(chosen_action)
-        this_ep_history.append([s, a, r, s1])
+        this_ep_history.append([state, chosen_action, r, s1])
         reward_this_ep += r
         state = s1
 
         if d == True: # FIXME - updates only happen when done, which we don't always reach
             # Update the network
             this_ep_history = np.array(this_ep_history)
-            discounted_rewards = discount_rewards([:,2])
-            feed_dict = {agent_discounted_rewards : this_ep_history[:,2],
+            discounted_rewards = discount_rewards(this_ep_history[:,2])
+            feed_dict = {agent_discounted_rewards : discounted_rewards,
                          agent_chosen_actions : this_ep_history[:,1],
-                         agent_state_in : this_ep_history[:,0]}
+                         agent_state_in : np.vstack(this_ep_history[:,0])}
             grads = sess.run(agent_gradients, feed_dict = feed_dict)
             for idx, grad in enumerate(grads):
                 grad_buffer[idx] += grad
@@ -110,7 +109,8 @@ while ep < num_episodes:
             break
 
     if ep % 100 == 0:
-        print("Trained on {} episodes".format(i))
+        print("Trained on {} episodes".format(ep))
         print("Mean reward: {}".format(np.mean(reward_history[-100:])))
+        print("Mean length: {}".format(np.mean(length_history[-100:])))
 
     ep += 1
